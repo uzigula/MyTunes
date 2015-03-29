f64657e3de219ce40ca72e5f8e2f7556927ffd6a
uzigula <uzi.mamani@gmail.com>
2015-03-28 19:11:31 -0500

    Refactoring SOLID Step3

--- src/MyTunes.Repository/CustomerRepository.cs
+++ src/MyTunes.Repository/CustomerRepository.cs
@@ -5,37 +5,10 @@ using MyTunes.Dominio;
 
 namespace MyTunes.Repository
 {
-    public class CustomerRepository : IRepository<Customer>
+    public class CustomerRepository : BaseRepository<Customer>
     {
-        private ChinookContext _context;
-        public CustomerRepository(DbContext context)
+        public CustomerRepository(DbContext context):base(context)
         {
-            _context = (ChinookContext)(context);
-        }
-        public Customer GetByEmail(string userName)
-        {
-            return _context.Customer.FirstOrDefault(x => x.Email == userName);
-        }
-
-        public void Dispose()
-        {
-            _context = null;
-        }
-
-        public IQueryable<Customer> Get()
-        {
-            return _context.Customer.AsQueryable();
-        }
-
-        public void Create(Customer entity)
-        {
-            _context.Customer.Add(entity);
-            _context.SaveChanges();
-        }
-
-        public void Update(Customer entity)
-        {
-            _context.SaveChanges();
         }
     }
 }
--- src/MyTunes.Repository/TrackRepository.cs
+++ src/MyTunes.Repository/TrackRepository.cs
@@ -5,35 +5,13 @@ using MyTunes.Dominio;
 
 namespace MyTunes.Repository
 {
-    public class TrackRepository : IRepository<Track>
+    public class TrackRepository : BaseRepository<Track>
     {
         private ChinookContext _context;
 
-        public TrackRepository(DbContext context)
+        public TrackRepository(DbContext context):base(context)
         {
-            _context = (ChinookContext)(context);
         }
 
-        public IQueryable<Track> Get()
-        {
-            return _context.Track.AsQueryable();
-        }
-
-        public void Dispose()
-        {
-            _context = null;
-        }
-
-
-        public void Create(Track entity)
-        {
-            _context.Track.Add(entity);
-            _context.SaveChanges();
-        }
-
-        public void Update(Track entity)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
