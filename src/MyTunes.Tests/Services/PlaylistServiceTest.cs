9b41c46803d381055d9ff227b0b6eb8975f138f6
uzigula <uzi.mamani@gmail.com>
2015-03-28 19:55:27 -0500

    Adding Test ServicesLayer

---
+++ src/MyTunes.Tests/Services/PlaylistServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MyTunes.Services;
+using NUnit.Framework;
+
+namespace MyTunes.Tests.Services
+{
+    [TestFixture]
+    public class PlaylistServiceTest
+    {
+        [Test]
+        [ExpectedException(typeof(InvalidOperationException), ExpectedMessage = "Cliente no encontrado Nonuser")]
+        public void GetlayLists_GivingAnInvalidUser_ReturnAnException()
+        {
+            var playListService = new PlayListService();
+            var playlists = playListService.GetPlayLists("NonUser");
+
+        }
+    }
+}
