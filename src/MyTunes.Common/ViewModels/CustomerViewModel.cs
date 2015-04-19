4d78bd20da2fc387687b86789ccf8c902c3263e8
uzigula <uzigula@gmail.com>
2015-04-19 09:00:59 -0500

    bad code

--- src/MyTunes.Common/ViewModels/CustomerViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyTunes.Common.ViewModels
-{
-    public class CustomerViewModel
-    {
-        public string Email { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-    }
-}
